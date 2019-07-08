703b39992d983fb346e7b1bdb1333070d98bb111
Cris <criston2011@gmail.com>
2019-07-08 15:21:14 +0530

    📦 NEW: Add list Items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todo</h1>
+        <TodoList />
       </header>
     </div>
   );
--- src/List.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const List = props => (
-	<ul>
-		{props.map.map((item, index) => (
-			<li key={index}>{item}</li>
-		))}
-	</ul>
-);
